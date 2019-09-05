323271bdbe28b20e298d96d0edf2d8bf0dba6d0f
Parker Faucher <parkerfaucher@Parkers-MacBook-Pro.local>
2019-09-05 15:15:02 -0700

    finished login and logout functionality along with turning a navigation bar into a smart component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,82 +1,100 @@
 import React, { Fragment, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
 
-const Login = () => {
-    const [formData, setFormData] = useState({
-        email: "",
-        password: ""
-      });
-    
-      const { email, password } = formData;
-    
-      const onChange = e =>
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    
-      const onSubmit = async e => {
-        e.preventDefault();
-        
-            console.log('success');
-            
-        //   const newUser = {
-        //     name,
-        //     email,
-        //     password
-        //   };
-    
-        //   try {
-        //     const config = {
-        //       headers: {
-        //         "Content-Type": "application/json"
-        //       }
-        //     };
-    
-        //     const body = JSON.stringify(newUser);
-    
-        //     const res = await axios.post("/api/users", body, config);
-        //     console.log(res.data);
-        //   } catch (err) {
-        //     console.log(err.response.data);
-        //   }
-        }
-      
-      return (
-        <Fragment>
-          {" "}
-          <h1 className="large text-primary">Sign In</h1>
-          <p className="lead">
-            <i className="fas fa-user"></i> Sigh Into Your Account
-          </p>
-          <form className="form" onSubmit={e => onSubmit(e)}>
-            <div className="form-group">
-              <input
-                type="email"
-                placeholder="Email Address"
-                name="email"
-                value={email}
-                onChange={e => onChange(e)}
-                required
-              />
-              
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={e => onChange(e)}
-                minLength="6"
-              />
-            </div>
-            
-            <input type="submit" className="btn btn-primary" value="Login" />
-          </form>
-          <p className="my-1">
-            Don't have an account? <Link to="/register">Sign Up</Link>
-          </p>
-        </Fragment>
-      );
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    login(email, password);
+
+    //   const newUser = {
+    //     name,
+    //     email,
+    //     password
+    //   };
+
+    //   try {
+    //     const config = {
+    //       headers: {
+    //         "Content-Type": "application/json"
+    //       }
+    //     };
+
+    //     const body = JSON.stringify(newUser);
+
+    //     const res = await axios.post("/api/users", body, config);
+    //     console.log(res.data);
+    //   } catch (err) {
+    //     console.log(err.response.data);
+    //   }
+  };
+
+  //redirect if logged in
+  if(isAuthenticated) {
+    return <Redirect to="/dashboard" />
+  }
+
+  return (
+    <Fragment>
+      {" "}
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sigh Into Your Account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -22,7 +24,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("passwords do not match", "danger");
     } else {
-      console.log("success");
+      register({ name, email, password });
 
       //   const newUser = {
       //     name,
@@ -46,6 +48,12 @@ const Register = ({ setAlert }) => {
       //   }
     }
   };
+
+  //redirect if logged in
+  if(isAuthenticated) {
+    return <Redirect to="/dashboard" />
+  }
+
   return (
     <Fragment>
       {" "}
@@ -61,7 +69,7 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={e => onChange(e)}
-            required
+            
           />
         </div>
         <div className="form-group">
@@ -71,7 +79,7 @@ const Register = ({ setAlert }) => {
             name="email"
             value={email}
             onChange={e => onChange(e)}
-            required
+            
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -85,7 +93,7 @@ const Register = ({ setAlert }) => {
             name="password"
             value={password}
             onChange={e => onChange(e)}
-            minLength="6"
+            
           />
         </div>
         <div className="form-group">
@@ -95,7 +103,7 @@ const Register = ({ setAlert }) => {
             name="password2"
             value={password2}
             onChange={e => onChange(e)}
-            minLength="6"
+            
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -108,10 +116,17 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-  null,
-  { setAlert }
+  mapStateToProps,
+  { setAlert,
+  register }
 )(Register);
